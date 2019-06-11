d601af5d8d3b84847e64ea786cf3f1ea7834d594
zzeratul zzeratul <Talfar2019@outlook.com>
2019-06-11 19:35:56 +0300

    Добавьте файлы проекта.

---
+++ MyConsoleMultiThreadTarget/MyTarget.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace MyConsoleMultiThreadTarget
+{
+    class MyTarget: AsyncTaskTarget
+    {
+        protected override Task WriteAsyncTask()
+        {
+
+        }
+    }
+}
